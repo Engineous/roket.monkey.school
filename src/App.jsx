3167892042fb3bd8fb5c3e4b6a04a1a336b10e90
Engineous <46768195+Engineous@users.noreply.github.com>
2024-01-06 20:56:59 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import roket from './assets/roket.png'
+
+function App() {
+
+  return (
+    <>
+      <h1 style={{textAlign: 'center'}}>ROKET</h1>
+      <img src={roket} />
+    </>
+  )
+}
+
+export default App
